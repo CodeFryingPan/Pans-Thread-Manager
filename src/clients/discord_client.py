acaf4bb5343ea439891a7efd338f2c6163bfb1d8
Philip Tam <philip.tam.code@gmail.com>
2021-11-25 15:36:59 -0500

    Completed Embed Posts with options

--- src/clients/discord_client.py
+++ src/clients/discord_client.py
@@ -3,13 +3,15 @@ import discord
 import os
 
 class DiscordClient(discord.Client):
-    def __init__(self, thread_channels, thread_emojis, thread_role, *args, **kwargs):
+    def __init__(self, thread_channels, thread_emojis, thread_role, embed_posts = False, *args, **kwargs):
         super().__init__(*args, **kwargs)
         
         self.thread_to_remove = dict()
         self.thread_channels = thread_channels.copy()
         self.thread_emojis = thread_emojis.copy()
         self.thread_role = thread_role
+        
+        self.embed_posts = embed_posts
 
         self.format = """--------
 **TITLE** (Atleast 10 characters long + CTRL+B or COMMAND+B)            
@@ -18,10 +20,11 @@ CONTENT (Alteast 30 characters long)
 --------"""
         self.footer = "How to bold title in markdown: **title**"
 
-    def _format_embed(self, message, title, user_posted = False, footer = None):
+    def _format_embed(self, message, title, user = None, footer = None):
         embed = discord.Embed(title=title, description=message, color=0xFFFFFF)
-        if user_posted:
-            embed.set_author(name=self.user.name, icon_url=self.user.avatar.url)
+        if user:
+            embed.add_field(name="Author", value="{}".format(user.mention), inline=True)
+            embed.set_author(name=user.name, icon_url=user.avatar.url)
         if footer:
             embed.set_footer(text=footer)
         return embed
@@ -30,21 +33,21 @@ CONTENT (Alteast 30 characters long)
         lines = content.split("\n")
 
         if len(lines) < 3:
-            return False, "Post does not follow the post format."
+            return False, "Post does not follow the post format.", None
         
         title, text = lines[0], lines[1: len(lines)]
 
         if len(title) < 14:
-            return False, "Title needs to be atleast 10 characters long!"
+            return False, "Title needs to be atleast 10 characters long!", None
         
         if title[:2] != "**" or title[-2:] != "**":
-            return False, "Title needs to be bolded!"
+            return False, "Title needs to be bolded!", None
 
         joined_text = " ".join(text)
         if len(joined_text) < 20:
-            return False, "Content needs to be atleast 20 characters long!"
+            return False, "Content needs to be atleast 20 characters long!", None
 
-        return True, title[2:-2]
+        return True, title[2:-2], "\n".join(text)
 
     async def on_ready(self):
         print('BOT IS ONLINE as {0}!'.format(self.user))
@@ -67,16 +70,30 @@ CONTENT (Alteast 30 characters long)
                 return
 
             if message.type == discord.MessageType.default:
-                valid, status = self._validate_format(content)
+                valid, status, text = self._validate_format(content)
                 if valid:
-                    for emoji_id in self.thread_emojis:
-                        emoji = discord.utils.get(self.emojis, id=emoji_id)
-                        if emoji == None: # Does not exist in the guild
-                            continue
-                        await message.add_reaction(emoji)
-                    await message.create_thread(name=status)
-                    role = discord.utils.get(message.guild.roles, id=self.thread_role)
-                    await author.add_roles(role)
+                    if self.embed_posts:
+                        embed = self._format_embed(title=status, message=text, user=author)
+                        embed_sent = await channel.send(embed=embed)
+                        for emoji_id in self.thread_emojis:
+                            emoji = discord.utils.get(self.emojis, id=emoji_id)
+                            if emoji == None: # Does not exist in the guild
+                                continue
+                            await embed_sent.add_reaction(emoji)
+                        thread = await embed_sent.create_thread(name=status)
+                        await thread.add_user(author)
+                        role = discord.utils.get(message.guild.roles, id=self.thread_role)
+                        await author.add_roles(role)
+                        await message.delete()
+                    else:
+                        for emoji_id in self.thread_emojis:
+                            emoji = discord.utils.get(self.emojis, id=emoji_id)
+                            if emoji == None: # Does not exist in the guild
+                                continue
+                            await message.add_reaction(emoji)
+                        await message.create_thread(name=status)
+                        role = discord.utils.get(message.guild.roles, id=self.thread_role)
+                        await author.add_roles(role)
                 else:
                     await message.delete()
                     embed = self._format_embed(title="Wrong Format", message="{} the message you sent does not pass post validation for a thread: \n__**{}**__ \n\n**Message:** \n{} \n\n\n **Please make it into a thread with the format:** \n{}".format(
