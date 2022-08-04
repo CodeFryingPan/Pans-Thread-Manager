a5276a90cda1d999a2d05646f2c86d40597f8616
Philip Tam <philip.tam.code@gmail.com>
2022-08-03 21:33:44 -0400

    Updated run and discord client

--- src/clients/discord_clientV2.py
+++ src/clients/discord_clientV2.py
@@ -8,7 +8,7 @@ import sys
 
 class DiscordClientV2(discord.Client):
     def __init__(self, threads, *args, **kwargs):
-        super().__init__( intents= discord.Intents.default(), *args, **kwargs)        
+        super().__init__( intents= [discord.Intents.default(), discord.Intents.message_content], *args, **kwargs)        
         self.thread_channels = dict()
         self.thread_to_remove = dict()
 
